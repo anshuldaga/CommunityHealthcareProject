d289c505a0c50e8dba420ca64e22df3b82d43065
sbharat99 <sbharat99@gmil.com>
2019-07-10 13:37:31 -0400

    Styled up Health Log

--- CommunityHealthcareApp/src/app/health-log/health-log.page.ts
+++ CommunityHealthcareApp/src/app/health-log/health-log.page.ts
@@ -10,6 +10,30 @@ export class HealthLogPage implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    
   }
+
+  private products = [
+    {
+      name: "Insulin Log",
+      notes: "Take once daily"
+    },
+  
+    {
+      name: "Blood Pressure Log",
+      notes: "Take BP Daily. Avoid taking after physical activity"
+    },
+  
+    {
+      name:"Blood Glucose Log",
+      notes:"Take BG before and after each meal"
+    },
+  
+    {
+      name:"Water Intake Log",
+      notes:"Hydrate after intense physical exercise"
+    }
+  ];
+  
 
 }
