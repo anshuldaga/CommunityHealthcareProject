2a35c023ae4fd2034b17468946da0b9cac63f373
ashish daga <ashishdaga@hotmail.com>
2019-10-08 20:08:39 -0400

    fixed script syntax or missing statements

--- DBScripts/04_usercredentialsDBdata.sql
+++ DBScripts/04_usercredentialsDBdata.sql
@@ -4,6 +4,8 @@ LIMIT 0, 1000
 
 -- Date: 2019-09-29 20:30
 */
+
+USE `HealthCareApp`;
 INSERT INTO `usercredentials` (`id`,`username`,`password`, `lastUpdatedDT`) VALUES (877,'usd','pasd', '2019-09-27');
 INSERT INTO `usercredentials` (`id`,`username`,`password`, `lastUpdatedDT`) VALUES (4321,'test_user2', 'test_password2', '2019-09-28');
 
@@ -12,5 +14,7 @@ INSERT INTO `usercredentials` (`id`,`username`,`password`, `lastUpdatedDT`) VALU
 INSERT INTO `usercredentials` (`id`,`username`,`password`, `lastUpdatedDT`) VALUES (8778,'usd','pasd', '2019-09-27');
 
 INSERT INTO `usercredentials` (`id`,`username`,`password`, `lastUpdatedDT`) VALUES (8779,'usd','pasd', '2019-09-27');
+
+INSERT INTO `usercredentials` (`id`,`username`,`password`, `lastUpdatedDT`) VALUES (87710,'ashish','ilvenasa', '2019-09-27');
 
 
