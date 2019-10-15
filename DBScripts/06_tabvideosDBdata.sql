2e814d667b40926826a9b6f6b30d0b4f26642501
ashish daga <ashishdaga@hotmail.com>
2019-10-14 22:42:26 -0400

    Corrected scripts with missing syntaxs
    Corrected scripts with missing syntaxs

--- DBScripts/06_tabvideosDBdata.sql
+++ DBScripts/06_tabvideosDBdata.sql
@@ -4,6 +4,7 @@ LIMIT 0, 1000
 
 -- Date: 2019-09-29 20:28
 */
+USE `HealthCareApp`;
 INSERT INTO `tabvideos` (`vid_id`,`tab_id`,`vid_title`,`vid_description`,`vid_link`) VALUES (1,1,'Why Is It So Hard To Switch From Apple to Android?','iPhone users have a hard time switching over to Android because they\'re inteeducationtabsgrated into Apple\'s ecosystem. Apple\'s software, products, and accessories locked in. However, Google is making the switch to Android a little easier.','https://www.youtube.com/embed/vyElbR6BzZ8');
 INSERT INTO `tabvideos` (`vid_id`,`tab_id`,`vid_title`,`vid_description`,`vid_link`) VALUES (2,1,'23 iPhone Tricks To Make Your Life Easier','The iPhone can do a lot of things, but many of its best features are buried in various menus you may not even know about. From perfect overhead shots to a hidden dark mode, here are some of our favorite iPhone tricks.','https://www.youtube.com/embed/U52mI00gmPU');
 INSERT INTO `tabvideos` (`vid_id`,`tab_id`,`vid_title`,`vid_description`,`vid_link`) VALUES (3,2,'There Are Gravity Pulses Hiding in the Universe’s Most MASSIVE Stars','Blue supergiant stars some of the hottest, biggest young stars in the universe, and their molten cores can reveal more about the formation of everything.','https://www.youtube.com/embed/wI-vP1_JnNM');
