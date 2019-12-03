b1ba9adda4e693b2ea57d4575b0034eac064bc3d
Rachana <43420002+iracoon@users.noreply.github.com>
2019-12-03 12:53:20 -0600

    passwords update

--- API/server/controllers/userCtrl.js
+++ API/server/controllers/userCtrl.js
@@ -4,55 +4,68 @@ const jwt = require('jsonwebtoken');
 var mysqlConnection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'MyCubical@11',
+  password: 'password',
   database: 'healthcareapp',
   multipleStatements: true
 });
 
 exports.createUser = (req, res, next) => {
-    let user = req.body;
-    var checkUsernameSql = 'SELECT COUNT(*) AS count FROM usercredentials WHERE username = ?;';
-    mysqlConnection.query(checkUsernameSql, [user.username], (err, rows) => {
-      if(err){
-        res.status(400).send("Error occurred");
-      }
-      else{
-        if(rows[0].count > 0){
-          return res.status(400).send("User already registered.");
-        }
-        else{
-          var sql = 'INSERT INTO usercredentials(firstName, lastName, address, username, password, lastUpdatedDT) VALUES (?, ?, ?, ?, ?, now());';
-          mysqlConnection.query(sql, [user.firstName, user.lastName, user.address, user.username, user.password], (err, rows, fields)=>{
-              if(!err){
-                let id = res.insertId;
-                let token = jwt.sign({ id }, 'jk23!+!97', { expiresIn: '1h' });
-                res.status(200).send({ token });
-              }
-              else{
-                return res.status(500).send("There was a problem registering the user");
-              }
-            });
+  let user = req.body;
+  var checkUsernameSql =
+    'SELECT COUNT(*) AS count FROM usercredentials WHERE username = ?;';
+  mysqlConnection.query(checkUsernameSql, [user.username], (err, rows) => {
+    if (err) {
+      res.status(400).send('Error occurred');
+    } else {
+      if (rows[0].count > 0) {
+        return res.status(400).send('User already registered.');
+      } else {
+        var sql =
+          'INSERT INTO usercredentials(firstName, lastName, address, username, password, lastUpdatedDT) VALUES (?, ?, ?, ?, ?, now());';
+        mysqlConnection.query(
+          sql,
+          [
+            user.firstName,
+            user.lastName,
+            user.address,
+            user.username,
+            user.password
+          ],
+          (err, rows, fields) => {
+            if (!err) {
+              let id = res.insertId;
+              let token = jwt.sign({ id }, 'jk23!+!97', { expiresIn: '1h' });
+              res.status(200).send({ token });
+            } else {
+              return res
+                .status(500)
+                .send('There was a problem registering the user');
+            }
           }
-        }
-    });
-}
+        );
+      }
+    }
+  });
+};
 
 exports.loginUser = (req, res) => {
   let user = req.body;
-  var sql = 'SELECT * FROM usercredentials WHERE username = ? AND password = ?;';
-  mysqlConnection.query(sql, [user.username, user.password], function(err, rows) {
+  var sql =
+    'SELECT * FROM usercredentials WHERE username = ? AND password = ?;';
+  mysqlConnection.query(sql, [user.username, user.password], function(
+    err,
+    rows
+  ) {
     if (err) {
-      res.status(400).send("Error occurred");
-    } 
-    else {
-      if(rows.length > 0){
-        if(rows[0].password == user.password){
+      res.status(400).send('Error occurred');
+    } else {
+      if (rows.length > 0) {
+        if (rows[0].password == user.password) {
           const id = rows[0].id;
           let token = jwt.sign({ id }, 'jk23!+!97', { expiresIn: '1h' });
           res.status(200).send({ token });
         }
-      }
-      else{
+      } else {
         res.status(400).send("Username and/or password doesn't match");
       }
     }
