941b3d695e28f444ed5e498b55a9a2bf7e888745
Rachana <43420002+iracoon@users.noreply.github.com>
2019-12-29 10:42:40 -0600

    Health Information new user bug fix

--- CommunityHealthcareApp/src/app/health-card/information/edit-information/edit-information.page.ts
+++ CommunityHealthcareApp/src/app/health-card/information/edit-information/edit-information.page.ts
@@ -1,64 +1,72 @@
 import { Component, OnInit, ViewChild, OnDestroy } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { InformationService } from '../information.service';
-import {Information} from '../information.model';
-import {Subscription} from 'rxjs';
+import { Information } from '../information.model';
+import { Subscription } from 'rxjs';
 import { Router } from '@angular/router';
 import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-edit-information',
   templateUrl: './edit-information.page.html',
-  styleUrls: ['./edit-information.page.scss'],
+  styleUrls: ['./edit-information.page.scss']
 })
 export class EditInformationPage implements OnInit, OnDestroy {
   @ViewChild('f') form: NgForm;
   loadedInformation: Information;
   private loadedInformationSub: Subscription;
 
-  constructor(private informationService: InformationService,
+  constructor(
+    private informationService: InformationService,
     private router: Router,
-    private loadingCtrl: LoadingController) {}
+    private loadingCtrl: LoadingController
+  ) {}
 
-    
   ionViewWillEnter() {
     this.informationService.fetchInformation().subscribe();
   }
 
-  ngOnInit(){
-    this.loadedInformationSub = this.informationService.information.subscribe(information => {
-      this.loadedInformation = information;
-    });
+  ngOnInit() {
+    this.loadedInformationSub = this.informationService.information.subscribe(
+      information => {
+        this.loadedInformation = information;
+      }
+    );
   }
 
-  ngOnDestroy(){
-    if(this.loadedInformationSub){
+  ngOnDestroy() {
+    if (this.loadedInformationSub) {
       this.loadedInformationSub.unsubscribe();
     }
   }
 
-  onInformationEntered(){
-    if(!this.form.valid){
+  onInformationEntered() {
+    if (!this.form.valid) {
       return;
     }
-    this.loadingCtrl.create({
-      message: 'Updating...'
-    }).then(loadingEl => {
-      loadingEl.present();
-      this.informationService.updateInformation(this.form.value['height_feet'], 
-      this.form.value['height_inches'],
-      this.form.value['weight'],
-      this.form.value['bloodtype'],
-      this.form.value['primary_contact'],
-      this.form.value['secondary_contact'],
-      this.form.value['medical_insurance'],
-      this.form.value['dental_insurance'],
-      this.form.value['birthday'],
-      this.form.value['allergy_notes']).subscribe(() => 
-      {
-        loadingEl.dismiss();
-        this.router.navigate(['tabs/tabs/information']);
+    this.loadingCtrl
+      .create({
+        message: 'Updating...'
+      })
+      .then(loadingEl => {
+        loadingEl.present();
+        this.informationService
+          .updateInformation(
+            this.form.value['height_feet'],
+            this.form.value['height_inches'],
+            this.form.value['weight'],
+            this.form.value['bloodtype'],
+            this.form.value['primary_contact'],
+            this.form.value['secondary_contact'],
+            this.form.value['medical_insurance'],
+            this.form.value['dental_insurance'],
+            this.form.value['birthday'],
+            this.form.value['allergy_notes']
+          )
+          .subscribe(() => {
+            loadingEl.dismiss();
+            this.router.navigate(['tabs/tabs/information']);
+          });
       });
-    });
   }
 }
--- CommunityHealthcareApp/src/app/health-card/information/information.service.ts
+++ CommunityHealthcareApp/src/app/health-card/information/information.service.ts
@@ -1,49 +1,91 @@
 import { Injectable } from '@angular/core';
 import { Information } from './information.model';
 import { BehaviorSubject, of } from 'rxjs';
-import {take, tap, switchMap} from 'rxjs/operators';
+import { take, tap, switchMap } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-
 export class InformationService {
   constructor(private http: HttpClient) {}
 
-  public information = new BehaviorSubject<Information>(new Information(null, null, null, null, null, null, null,
-    null, null, null, null));
+  public information = new BehaviorSubject<Information>(
+    new Information(
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null
+    )
+  );
 
   fetchInformation() {
-    return this.http.get<{[key: string]: Information}>('http://localhost:3000/userhealth/').pipe(
-      take(1),
-      tap(res => {
-        if (!(Object.keys(res).length === 0)) {
-          this.information.next(new Information(res[0].userId, res[0].height_feet, res[0].height_inches, res[0].weight,
-            res[0].bloodtype, res[0].primary_contact, res[0].secondary_contact, res[0].medical_insurance,
-            res[0].dental_insurance, res[0].birthday, res[0].allergy_notes));
-        } else {
-          this.information.next(new Information(null, null, null, null, null, null, null,
-            null, null, null, null));
-        }
-    }));
+    return this.http
+      .get<{ [key: string]: Information }>('http://localhost:3000/userhealth/')
+      .pipe(
+        take(1),
+        tap(res => {
+          if (Object.keys(res) != null) {
+            this.information.next(
+              new Information(
+                res[0].userId,
+                res[0].height_feet,
+                res[0].height_inches,
+                res[0].weight,
+                res[0].bloodtype,
+                res[0].primary_contact,
+                res[0].secondary_contact,
+                res[0].medical_insurance,
+                res[0].dental_insurance,
+                res[0].birthday,
+                res[0].allergy_notes
+              )
+            );
+          } else {
+            this.information.next(
+              new Information(
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null
+              )
+            );
+          }
+        })
+      );
   }
 
-  updateInformation(height_feet: number,
-                    height_inches: number,
-                    weight: number,
-                    bloodtype: string,
-                    primary_contact: number,
-                    secondary_contact: number,
-                    medical_insurance: string,
-                    dental_insurance: string,
-                    birthday: string,
-                    allergy_notes: string) {
-   return this.information.pipe(
+  updateInformation(
+    height_feet: number,
+    height_inches: number,
+    weight: number,
+    bloodtype: string,
+    primary_contact: number,
+    secondary_contact: number,
+    medical_insurance: string,
+    dental_insurance: string,
+    birthday: string,
+    allergy_notes: string
+  ) {
+    return this.information.pipe(
       take(1),
-        switchMap(information => {
-          information = {
+      switchMap(information => {
+        information = {
           userId: null,
           height_feet,
           height_inches,
@@ -56,13 +98,25 @@ export class InformationService {
           birthday,
           allergy_notes
         };
-          return this.http.put('http://localhost:3000/userhealth/', information);
-      }), tap(() => {
-        this.information.next(new Information(null, height_feet, height_inches, weight,
-        bloodtype, primary_contact, secondary_contact, medical_insurance, dental_insurance,
-        birthday, allergy_notes));
+        return this.http.put('http://localhost:3000/userhealth/', information);
+      }),
+      tap(() => {
+        this.information.next(
+          new Information(
+            null,
+            height_feet,
+            height_inches,
+            weight,
+            bloodtype,
+            primary_contact,
+            secondary_contact,
+            medical_insurance,
+            dental_insurance,
+            birthday,
+            allergy_notes
+          )
+        );
       })
-   );
+    );
   }
-
 }
