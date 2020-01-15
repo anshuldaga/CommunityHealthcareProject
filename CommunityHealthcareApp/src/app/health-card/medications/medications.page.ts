b92d8eb2c479c59a814cffe4ee71c417fd220e2a
Rachana <43420002+iracoon@users.noreply.github.com>
2020-01-15 09:59:12 -0600

    added edit option to health medications page

--- CommunityHealthcareApp/src/app/health-card/medications/medications.page.ts
+++ CommunityHealthcareApp/src/app/health-card/medications/medications.page.ts
@@ -11,7 +11,9 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./medications.page.scss']
 })
 export class MedicationsPage implements OnInit, OnDestroy {
-  public buttonText = 'delete';
+  public deleteButtonText = 'delete';
+  public editButtonText = 'edit';
+  public canEdit: boolean = false;
   public canDelete: boolean = false;
   loadedMedications: Medication[];
   private loadedMedicationsSub: Subscription;
@@ -41,8 +43,11 @@ export class MedicationsPage implements OnInit, OnDestroy {
   }
 
   onAddMedication() {
+    this.canEdit = false;
+    this.deleteButtonText = 'delete';
+    this.editButtonText = 'edit';
+    this.canEdit = false;
     this.canDelete = false;
-    this.buttonText = 'delete';
     this.modalCtrl
       .create({ component: AddMedicationComponent })
       .then(modalEl => {
@@ -65,11 +70,24 @@ export class MedicationsPage implements OnInit, OnDestroy {
   }
 
   onDeleteMedication() {
+    this.editButtonText = 'edit';
+    this.canEdit = false;
     this.canDelete = !this.canDelete;
-    if (this.buttonText === 'delete') {
-      this.buttonText = 'done';
+    if (this.deleteButtonText === 'delete') {
+      this.deleteButtonText = 'done';
     } else {
-      this.buttonText = 'delete';
+      this.deleteButtonText = 'delete';
+    }
+  }
+
+  onEditMedication() {
+    this.deleteButtonText = 'delete';
+    this.canDelete = false;
+    this.canEdit = !this.canEdit;
+    if (this.editButtonText === 'edit') {
+      this.editButtonText = 'done';
+    } else {
+      this.editButtonText = 'edit';
     }
   }
 }
