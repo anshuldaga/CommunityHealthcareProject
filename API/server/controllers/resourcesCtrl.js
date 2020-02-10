5178648f98c7872c8fe4a44fa3bd4c4263501ada
Rachana <43420002+iracoon@users.noreply.github.com>
2020-02-10 13:03:11 -0600

    resources query updated

--- API/server/controllers/resourcesCtrl.js
+++ API/server/controllers/resourcesCtrl.js
@@ -7,10 +7,14 @@ var mysqlConnection = require('../mysqlConnection');
 
 exports.getResources = (req, res, next) => {
   mysqlConnection.query(
-    'SELECT categories.category_title, resources.all_details\
-    FROM categories \
-        LEFT JOIN ( SELECT category_id, JSON_ARRAYAGG(JSON_OBJECT("resource_title", resource_title, "resource_description", resource_description)) all_details FROM resources GROUP BY category_id\
-        ) resources ON resources.category_id = categories.category_id',
+    'SELECT categories.category_id, \
+    JSON_ARRAYAGG(JSON_OBJECT("resource_title", resource_title, \
+    "resource_description", resource_description, \
+    "all_details",  JSON_OBJECT("state", state, "city", city)) ) all_resources \
+    FROM categories, resources, resource_details \
+    WHERE categories.category_id = resources.category_id \
+    AND resource_details.resource_id = resources.resource_id \
+    GROUP BY categories.category_id',
     (err, rows, fields) => {
       if (!err) res.send(rows);
       else console.log(err);
