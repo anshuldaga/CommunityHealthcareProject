6c290b068ea9f1d900386e71c688c9708552e1bf
Rachana <43420002+iracoon@users.noreply.github.com>
2020-02-10 13:50:29 -0600

    added resources listings page in each category

--- CommunityHealthcareApp/src/app/health-resources/all-resources/all-resources.page.ts
+++ CommunityHealthcareApp/src/app/health-resources/all-resources/all-resources.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HealthResourcesService } from '../health-resources.service';
 
 @Component({
   selector: 'app-all-resources',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./all-resources.page.scss'],
 })
 export class AllResourcesPage implements OnInit {
+  all_resources: any;
+  loadedTab: any;
 
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private healthResourcesService: HealthResourcesService,
+  ) {}
 
   ngOnInit() {
+    this.activatedRoute.paramMap.subscribe(paramMap => {
+      if (!paramMap.has('category')) {
+        return;
+      }
+      const category = paramMap.get('category');
+      this.loadedTab = this.healthResourcesService.getResourcesTab(
+        category
+      );
+      this.all_resources = eval(this.loadedTab.all_resources);
+    });
   }
-
 }
