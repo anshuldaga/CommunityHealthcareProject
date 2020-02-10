aa59689106221bcef6cf9264827bfbc748c87bc9
Rachana <43420002+iracoon@users.noreply.github.com>
2020-02-10 11:13:24 -0600

    resourced query

--- API/server/controllers/resourcesCtrl.js
+++ API/server/controllers/resourcesCtrl.js
@@ -7,10 +7,11 @@ var mysqlConnection = require('../mysqlConnection');
 
 exports.getResources = (req, res, next) => {
   mysqlConnection.query(
-    'SELECT categories.category_title, resources.resource_title\
+    'SELECT categories.category_title, resources.listings\
       FROM categories \
-          LEFT JOIN ( SELECT tab_id, JSON_ARRAYAGG(JSON_OBJECT("vid_title", vid_title, "vid_description", vid_description, "vid_link", vid_link)) vidSection FROM tabvideos GROUP BY tab_id\
-          ) tabvideos ON tabvideos.tab_id = educationtabs.tab_id',
+          LEFT JOIN ( SELECT category_id, JSON_ARRAYAGG(JSON_OBJECT("resource_title", \
+          resource_title, "resource_description", resource_description)) listings FROM resources GROUP BY category_id\
+          ) resources ON resources.category_id = categories.category_id',
     (err, rows, fields) => {
       if (!err) res.send(rows);
       else console.log(err);
